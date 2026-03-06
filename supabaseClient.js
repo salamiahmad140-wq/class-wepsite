f8bb4c456aced10368512ad768eeb67d1c691214
salamiahmad140-wq <salamiahmad140@gmail.com>
2026-03-06 04:48:19 +0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// الرابط الخاص بمشروعك كما ظهر في الصور
 const supabaseUrl = 'https://wvygiylllzoyovhoviya.supabase.co'
-
-// ضعي هنا الـ Publishable key (anon) الذي يبدأ بـ ey...
-const supabaseAnonKey = 'sb_publishable_wnuFR5mOyyDDf-U23Bm9tw_ge5hXObn'
+const supabaseAnonKey = 'sb_publishable_wnuFR5mOyyDDf-U23Bm9tw_ge5hXObn' 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
