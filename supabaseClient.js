86e03fe19c0afb6bc64d0d9369ab20bc49855e64
salamiahmad140-wq <salamiahmad140@gmail.com>
2026-03-06 04:46:20 +0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,9 @@
-import { createClient } from '@supabase/supabase-client'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// استبدلي هذه القيم من إعدادات Supabase التي وجدتها
-const supabaseUrl = 'https://wvygiylllzoyovhoviya.supabase.co' 
+// الرابط الخاص بمشروعك كما ظهر في الصور
+const supabaseUrl = 'https://wvygiylllzoyovhoviya.supabase.co'
+
+// ضعي هنا الـ Publishable key (anon) الذي يبدأ بـ ey...
 const supabaseAnonKey = 'sb_publishable_wnuFR5mOyyDDf-U23Bm9tw_ge5hXObn'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
